ff77fee0f5e2ad0c998cf69c890d659405c39dff
--local <masterking.cs@gmail.com>
2014-05-02 21:23:42 -0400

    Customized Loading page

--- Parkhour/main.js
+++ Parkhour/main.js
@@ -2,7 +2,8 @@ cc.game.onStart = function(){
     cc.view.setDesignResolutionSize(480, 320, cc.ResolutionPolicy.SHOW_ALL);
 	cc.view.resizeWithBrowserSize(true);
     //load resources
-    cc.LoaderScene.preload(g_resources, function () {
+    //cc.LoaderScene.preload(g_resources, function () {
+    Loading.preload(g_resources, function () {
         cc.director.runScene(new HelloWorldScene());
     }, this);
 };
