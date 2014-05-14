73f05c2797d9acc148ec36e06e0fa6112967bd8c
--local <masterking.cs@gmail.com>
2014-05-13 22:08:10 -0400

    LouliRun

---
+++ LuoLiRun/Classes/GameMenuScene.cpp
@@ -0,0 +1,9 @@
+//
+//  GameMenuScene.cpp
+//  LuoLiRun
+//
+//  Created by iMac on 2014-05-12.
+//
+//
+
+#include "GameMenuScene.h"
---
+++ LuoLiRun/Classes/GameMenuScene.h
@@ -0,0 +1,14 @@
+//
+//  GameMenuScene.h
+//  LuoLiRun
+//
+//  Created by iMac on 2014-05-12.
+//
+//
+
+#ifndef __LuoLiRun__GameMenuScene__
+#define __LuoLiRun__GameMenuScene__
+
+#include <iostream>
+
+#endif /* defined(__LuoLiRun__GameMenuScene__) */
